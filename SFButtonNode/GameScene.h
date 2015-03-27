2c8d0dc96ad2d8b49ea85611f8da7b509c859df8
Skye <gskye@aol.com>
2015-03-27 17:53:53 -0400

    Initial Commit

---
+++ SFButtonNode/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  SFButtonNode
+//
+
+//  Copyright (c) 2015 Skye Freeman. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ SFButtonNode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SFButtonNode
+//
+//  Created by Skye on 3/27/15.
+//  Copyright (c) 2015 Skye Freeman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
